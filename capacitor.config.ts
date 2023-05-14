8c32c0af60dd97973aa89d84bae691314e6161b3
Aaron Saunders <aaronksaunders@gmail.com>
2023-05-14 18:50:25 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'storybook-ionic-angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
